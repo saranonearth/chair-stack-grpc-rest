2d8158472714f9cf39b4eb6d8e83c2138b405035
saranonearth <saran.suresh@outlook.com>
2020-09-06 15:52:52 +0530

    Client UI added

--- grpc/client/app.js
+++ grpc/client/app.js
@@ -1,16 +1,1 @@
-import {
-    AddServiceClient,
-    Item,
-    ItemList,
-} from "./proto/addGarment_grpc_web_pb";
-
-const addclient = new AddServiceClient("http://localhost:9003");
-
-let req = new Item();
-
-req.setGarment("TShirt");
-
-addclient.addGarment(req, {}, (err, res) => {
-    console.log(err);
-    console.log(res.getListList());
-});
+console.log("gRPC Client Running")
