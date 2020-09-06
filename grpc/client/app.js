9c09c379348fb86fc69df01ca66f36c6d103570a
saranonearth <saran.suresh@outlook.com>
2020-09-06 21:46:27 +0530

    client app.js refactored

--- grpc/client/app.js
+++ grpc/client/app.js
@@ -1,175 +1,120 @@
-import { GarmentServiceClient, GetAllRequest, Item, ClearRequest } from './proto/garmentActions_grpc_web_pb'
-
-
-/* 
-Connecting to the GarmentService by creating a instance
-of GarmentServiceClient
-*/
-
-const garmentClient = new GarmentServiceClient("http://localhost:9003")
-
-
-
-console.log("gRPC Client Running")
+import {
+    GarmentServiceClient,
+    GetAllRequest,
+    Item,
+    ClearRequest,
+} from "./proto/garmentActions_grpc_web_pb";
+const garmentClient = new GarmentServiceClient("http://localhost:9003");
 const URI_TSHIRT = "https://i.ibb.co/qYz06y9/t-shirts.png";
-const URI_SWEATSHIRT = "https://i.ibb.co/bgKQYNh/tracksuit.png"
-const URI_PANT = "https://i.ibb.co/cbFffwV/pants.png"
-
-
-/*
- Util functions
-*/
-
+const URI_SWEATSHIRT = "https://i.ibb.co/bgKQYNh/tracksuit.png";
+const URI_PANT = "https://i.ibb.co/cbFffwV/pants.png";
 const giveMeListofObjects = (data) => {
     let result = [];
 
-    data.map(item => {
-        result = [{ id: item[0], garment: item[1] }, ...result]
-    })
+    data.map((item) => {
+        result = [{ id: item[0], garment: item[1] }, ...result];
+    });
 
-    return result
-}
-
-
-/*
-    Fetching all the garments
-*/
-
-const getall = document.getElementById('getall')
-
-getall.addEventListener('click', () => {
-    checkChair()
-})
-
-
-
+    return result;
+};
+const generateInnerHTML = (item, stack) => {
+    switch (item.garment) {
+        case "tshirt":
+            return (
+                stack.innerHTML +
+                `<div class="garment">
+                            <div><img class="icons shirt" src="${URI_TSHIRT}" alt=""></div>
+                        </div>`
+            );
+            break;
+        case "sweatshirt":
+            return (
+                stack.innerHTML +
+                `<div class="garment">
+                            <div><img class="icons shirt" src="${URI_SWEATSHIRT}" alt=""></div>
+                        </div>`
+            );
+            break;
+        case "pant":
+            return (
+                stack.innerHTML +
+                `<div class="garment">
+                            <div><img class="icons shirt" src="${URI_PANT}" alt=""></div>
+                        </div>`
+            );
+    }
+};
+const getall = document.getElementById("getall");
+getall.addEventListener("click", () => {
+    checkChair();
+});
 const checkChair = () => {
-
     let req = new GetAllRequest();
 
-
     garmentClient.allGarments(req, {}, (err, res) => {
-        if (err) return alert("ERROR while fetching garments!")
+        if (err) return alert("ERROR while fetching garments!");
 
         const data = giveMeListofObjects(res.array[0]);
 
-        console.log(data)
+        console.log(data);
 
-        const stack = document.getElementById('chairItems')
+        const stack = document.getElementById("chairItems");
         stack.innerHTML = "";
 
-        data.map(item => {
-            console.log(item.garment)
+        data.map((item) => {
+            console.log(item.garment);
 
-            switch (item.garment) {
-                case "tshirt":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_TSHIRT}" alt=""></div>
-                        </div>`;
-                    break;
-                case "sweatshirt":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_SWEATSHIRT}" alt=""></div>
-                        </div>`;
-                    break;
-                case "pant":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_PANT}" alt=""></div>
-                        </div>`;
-            }
-        })
-    })
-
-}
-
-
-/*
-    Adding garments
-*/
-
-const tshirt = document.getElementById("tshirt")
-const pant = document.getElementById("pant")
-const sweatshirt = document.getElementById("sweatshirt")
-
-
-tshirt.addEventListener('click', () => {
-    addGarment('tshirt')
-})
-sweatshirt.addEventListener('click', () => {
-    addGarment('sweatshirt')
-})
-
-pant.addEventListener('click', () => {
-    addGarment('pant')
-})
-
+            stack.innerHTML = generateInnerHTML(item, stack);
+        });
+    });
+};
+const tshirt = document.getElementById("tshirt");
+const pant = document.getElementById("pant");
+const sweatshirt = document.getElementById("sweatshirt");
+tshirt.addEventListener("click", () => {
+    addGarment("tshirt");
+});
+sweatshirt.addEventListener("click", () => {
+    addGarment("sweatshirt");
+});
+pant.addEventListener("click", () => {
+    addGarment("pant");
+});
 const addGarment = (item) => {
     let req = new Item();
     req.setGarment(item);
-
     garmentClient.addGarment(req, {}, (err, res) => {
-        if (err) return alert("ERROR while adding garment!")
+        if (err) return alert("ERROR while adding garment!");
 
         const data = giveMeListofObjects(res.array[0]);
 
-        console.log(data)
+        console.log(data);
 
-        const stack = document.getElementById('chairItems')
+        const stack = document.getElementById("chairItems");
         stack.innerHTML = "";
 
-        data.map(item => {
-            console.log(item.garment)
-
-            switch (item.garment) {
-                case "tshirt":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_TSHIRT}" alt=""></div>
-                        </div>`;
-                    break;
-                case "sweatshirt":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_SWEATSHIRT}" alt=""></div>
-                        </div>`;
-                    break;
-                case "pant":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_PANT}" alt=""></div>
-                        </div>`;
-            }
-        })
-
-    })
-}
-
-/*
-    Doing laundry to clear the chair
-*/
-
-const wash = document.getElementById("wash")
-
-wash.addEventListener('click', () => {
-    console.log("Clicked wash")
-    clearChair()
-
-})
-
+        data.map((item) => {
+            console.log(item.garment);
+            stack.innerHTML = generateInnerHTML(item, stack);
+        });
+    });
+};
+const wash = document.getElementById("wash");
+wash.addEventListener("click", () => {
+    console.log("Clicked wash");
+    clearChair();
+});
 const clearChair = () => {
-    let req = new ClearRequest()
-
-
+    let req = new ClearRequest();
     garmentClient.clearList(req, {}, (err, res) => {
-        console.log(err)
-        console.log(res)
-    })
-
-    const stack = document.getElementById('chairItems')
+        console.log(err);
+        console.log(res);
+    });
+    const stack = document.getElementById("chairItems");
     stack.innerHTML = "";
-
-    const yay = document.getElementById('yay')
-
-    yay.style.display = 'block';
-
+    const yay = document.getElementById("yay");
+    yay.style.display = "block";
     setTimeout(() => {
-        yay.style.display = 'none'
-    }, 2000)
-}
+        yay.style.display = "none";
+    }, 2000);
+};
--- rest-api/client/app.js
+++ rest-api/client/app.js
@@ -1,152 +1,114 @@
-import axios from 'axios'
-
-/*Base endpoint of the REST api*/
-const BASE_ENDPOINT = "http://localhost:1366"
-
-
-console.log("REST Client Running")
+import axios from "axios";
+const BASE_ENDPOINT = "http://localhost:1366";
 const URI_TSHIRT = "https://i.ibb.co/qYz06y9/t-shirts.png";
-const URI_SWEATSHIRT = "https://i.ibb.co/bgKQYNh/tracksuit.png"
-const URI_PANT = "https://i.ibb.co/cbFffwV/pants.png"
-
-
-/*
-    Fetching all the garments
-*/
-
-const getall = document.getElementById('getall')
-
-getall.addEventListener('click', () => {
-    checkChair()
-})
-
-
-
+const URI_SWEATSHIRT = "https://i.ibb.co/bgKQYNh/tracksuit.png";
+const URI_PANT = "https://i.ibb.co/cbFffwV/pants.png";
+const generateInnerHTML = (item, stack) => {
+    switch (item.garment) {
+        case "tshirt":
+            return (
+                stack.innerHTML +
+                `<div class="garment">
+                            <div><img class="icons shirt" src="${URI_TSHIRT}" alt=""></div>
+                        </div>`
+            );
+            break;
+        case "sweatshirt":
+            return (
+                stack.innerHTML +
+                `<div class="garment">
+                            <div><img class="icons shirt" src="${URI_SWEATSHIRT}" alt=""></div>
+                        </div>`
+            );
+            break;
+        case "pant":
+            return (
+                stack.innerHTML +
+                `<div class="garment">
+                            <div><img class="icons shirt" src="${URI_PANT}" alt=""></div>
+                        </div>`
+            );
+    }
+};
+const getall = document.getElementById("getall");
+getall.addEventListener("click", () => {
+    checkChair();
+});
 const checkChair = () => {
+    axios
+        .get(`${BASE_ENDPOINT}/all-garments`)
+        .then((RESPONSE) => {
+            console.log("FETCH ALL GARMENTS RESPONSE", RESPONSE);
 
-    axios.get(`${BASE_ENDPOINT}/all-garments`).then(RESPONSE => {
-        console.log("FETCH ALL GARMENTS RESPONSE", RESPONSE)
+            let data = RESPONSE.data.reverse();
 
-        let data = RESPONSE.data.reverse();
+            const stack = document.getElementById("chairItems");
+            stack.innerHTML = "";
 
-        const stack = document.getElementById('chairItems')
-        stack.innerHTML = "";
+            data.map((item) => {
+                console.log(item.garment);
 
-        data.map(item => {
-            console.log(item.garment)
-
-            switch (item.garment) {
-                case "tshirt":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_TSHIRT}" alt=""></div>
-                        </div>`;
-                    break;
-                case "sweatshirt":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_SWEATSHIRT}" alt=""></div>
-                        </div>`;
-                    break;
-                case "pant":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_PANT}" alt=""></div>
-                        </div>`;
-            }
+                stack.innerHTML = generateInnerHTML(item, stack);
+            });
         })
-
-    }).catch(err => {
-        alert("Error while getting all garments", err)
-    })
-
-
-}
-
-
-/*
-    Adding garments
-*/
-
-const tshirt = document.getElementById("tshirt")
-const pant = document.getElementById("pant")
-const sweatshirt = document.getElementById("sweatshirt")
-
-
-tshirt.addEventListener('click', () => {
-    addGarment('tshirt')
-})
-sweatshirt.addEventListener('click', () => {
-    addGarment('sweatshirt')
-})
-
-pant.addEventListener('click', () => {
-    addGarment('pant')
-})
-
-
-
+        .catch((err) => {
+            console.log(err);
+            alert("Error while getting all garments", err);
+        });
+};
+const tshirt = document.getElementById("tshirt");
+const pant = document.getElementById("pant");
+const sweatshirt = document.getElementById("sweatshirt");
+tshirt.addEventListener("click", () => {
+    addGarment("tshirt");
+});
+sweatshirt.addEventListener("click", () => {
+    addGarment("sweatshirt");
+});
+pant.addEventListener("click", () => {
+    addGarment("pant");
+});
 const addGarment = (item) => {
+    axios
+        .get(`${BASE_ENDPOINT}/add-garment?garment=${item}`)
+        .then((RESPONSE) => {
+            console.log("ADD GARMENT RESPONSE ", RESPONSE);
+            let data = RESPONSE.data.reverse();
 
-    //Fetching from /add-garment
+            const stack = document.getElementById("chairItems");
+            stack.innerHTML = "";
 
-    axios.get(`${BASE_ENDPOINT}/add-garment?garment=${item}`).then(RESPONSE => {
-        console.log("ADD GARMENT RESPONSE ", RESPONSE)
-        let data = RESPONSE.data.reverse()
-
-        const stack = document.getElementById('chairItems')
-        stack.innerHTML = "";
-
-        data.map(item => {
-            switch (item.garment) {
-                case "tshirt":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_TSHIRT}" alt=""></div>
-                        </div>`;
-                    break;
-                case "sweatshirt":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_SWEATSHIRT}" alt=""></div>
-                        </div>`;
-                    break;
-                case "pant":
-                    stack.innerHTML = stack.innerHTML + `<div class="garment">
-                            <div><img class="icons shirt" src="${URI_PANT}" alt=""></div>
-                        </div>`;
-            }
+            data.map((item) => {
+                stack.innerHTML = generateInnerHTML(item, stack);
+            });
         })
-    }).catch(err => {
-        alert("Error while adding garment", err)
-    })
-
-
-}
-
-/*
-    Doing laundry to clear the chair
-*/
-
-const wash = document.getElementById("wash")
-
-wash.addEventListener('click', () => {
-    console.log("Clicked wash")
-    clearChair()
-
-})
-
+        .catch((err) => {
+            alert("Error while adding garment", err);
+        });
+};
+const wash = document.getElementById("wash");
+wash.addEventListener("click", () => {
+    console.log("Clicked wash");
+    clearChair();
+});
 const clearChair = () => {
+    axios
+        .get(`${BASE_ENDPOINT}/clear`)
+        .then((RESPONSE) => {
+            console.log(RESPONSE);
 
-    axios.get(`${BASE_ENDPOINT}/clear`).then(RESPONSE => {
-        console.log(RESPONSE)
+            const stack = document.getElementById("chairItems");
+            stack.innerHTML = "";
 
-        const stack = document.getElementById('chairItems')
-        stack.innerHTML = "";
+            const yay = document.getElementById("yay");
 
-        const yay = document.getElementById('yay')
+            yay.style.display = "block";
 
-        yay.style.display = 'block';
-
-        setTimeout(() => {
-            yay.style.display = 'none'
-        }, 2000)
-    }).catch(err => {
-        alert("Error while clearing", err)
-    })
-}
+            setTimeout(() => {
+                yay.style.display = "none";
+            }, 2000);
+        })
+        .catch((err) => {
+            alert("Error while clearing", err);
+        });
+};
